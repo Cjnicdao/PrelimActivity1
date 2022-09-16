03af785c7052b2a98efe45c6191f9f28885c98a1
KALVEN\user <cjnicdao@csmbataan.ph.education>
2022-09-16 11:56:32 +0800

    commit1

---
+++ CalvinNicdao.PrelimActivity1/CalvinNicdao.DateCalculator/Pages/Dates.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CalvinNicdao.DateCalculator.Pages
+{
+    public class DatesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
